b895600d57d9a4deb4346dbb57a4f1a074877db1
algowhiz <147070075+algowhiz@users.noreply.github.com>
2024-06-15 09:40:42 +0530

    Update context.jsx

--- src/context/context.jsx
+++ src/context/context.jsx
@@ -9,28 +9,25 @@ const gf = new GiphyFetch(import.meta.env.VITE_API_KEY);
 const GifProvider = ({ children }) => {
     const [gifs, setGifs] = useState([]);
     const [filter, setFilter] = useState('');
-    const [favorite, setFavorite] = useState([]);
-
-    const addToFav = ( id =>{
-        if(favorite?.includes(id)){
-            const updateFavList = favorite.filter((it)=> it!==id);
-            localStorage.setItem('favGif',JSON.stringify(updateFavList));
-            setFavorite(updateFavList);
-        }else{
-            const updatedFav=[...favorite];
-            updatedFav.push(id);
-            localStorage.setItem('favGif',JSON.stringify(updatedFav));
-            setFavorite(updatedFav);
-        }
+    const [favorite, setFavorite] = useState(() => {
+        const storedFavs = localStorage.getItem('favGif');
+        return storedFavs ? JSON.parse(storedFavs).filter(fav => fav !== null) : [];
     });
 
-    useEffect(()=>{
-        const favorite = JSON.parse(localStorage.getItem('favGif'));
-        setFavorite(favorite);
-    },[])
+    const addToFav = (id) => {
+        if (favorite?.includes(id)) {
+            const updateFavList = favorite.filter((it) => it !== id);
+            localStorage.setItem('favGif', JSON.stringify(updateFavList));
+            setFavorite(updateFavList);
+        } else {
+            const updatedFav = [...favorite, id];
+            localStorage.setItem('favGif', JSON.stringify(updatedFav));
+            setFavorite(updatedFav);
+        }
+    };
 
     return (
-        <GifContext.Provider value={{ gf, gifs, setGifs, filter, setFilter, favorite, setFavorite ,addToFav}}>
+        <GifContext.Provider value={{ gf, gifs, setGifs, filter, setFilter, favorite, setFavorite, addToFav }}>
             {children}
         </GifContext.Provider>
     );
